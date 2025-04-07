6c91a95075ad34e36e4670cbd61a12ebc305dfc4
xmosuma <xolanimosuma@gmail.com>
2025-04-07 09:54:06 +0200

    just created the skeleton

---
+++ user-service/src/main/java/com/Syntax_Squad_Inc/user_service/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Syntax_Squad_Inc.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
---
+++ user-service/src/test/java/com/Syntax_Squad_Inc/user_service/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Syntax_Squad_Inc.user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
